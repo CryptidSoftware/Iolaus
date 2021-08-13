fc480bc9c0733193f92012697c51b6f7adb1bf65
red <ryanwchild@gmail.com>
2021-08-12 20:43:07 -0600

    Fixing issue where NatsListener falls over when an exception falls over

--- src/Iolaus.Nats/NatsListener.cs
+++ src/Iolaus.Nats/NatsListener.cs
@@ -1,6 +1,7 @@
 using Iolaus.Observer;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using NATS.Client;
 using System;
@@ -16,10 +17,11 @@ namespace Iolaus.Nats
         private ObserverRouter _router;
         private IServiceScopeFactory _scopeFactory;
         private NatsListenerOptions _options;
+        private ILogger<NatsListener> _logger;
 
-        public NatsListener(NatsListenerServices services, IOptions<NatsListenerOptions> options) => 
-            (_connection, _router, _scopeFactory, _options) = 
-            (services.Connection, services.Router, services.ScopeFactory, options.Value);
+        public NatsListener(NatsListenerServices services, IOptions<NatsListenerOptions> options, ILogger<NatsListener> logger) => 
+            (_connection, _router, _scopeFactory, _options, _logger) = 
+            (services.Connection, services.Router, services.ScopeFactory, options.Value, logger);
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
@@ -30,11 +32,18 @@ namespace Iolaus.Nats
 
         private async void HandleMessage(object sender, MsgHandlerEventArgs args)
         {
-            using var scope = _scopeFactory.CreateScope();
-            var msg = Encoding.UTF8.GetString(args.Message.Data);
-            var message = Message.Parse(msg).Unsafe();
-            var handlerFunc = _router.GetFunc(scope.ServiceProvider, message);
-            await handlerFunc(message, BuildReply(args.Message));
+            try
+            {
+                using var scope = _scopeFactory.CreateScope();
+                var msg = Encoding.UTF8.GetString(args.Message.Data);
+                var message = Message.Parse(msg).Unsafe();
+                var handlerFunc = _router.GetFunc(scope.ServiceProvider, message);
+                await handlerFunc(message, BuildReply(args.Message));
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, $"Could not process message: {e.Message}");
+            }
         }
 
         private Func<Message,Task> BuildReply(Msg natsMessage)
